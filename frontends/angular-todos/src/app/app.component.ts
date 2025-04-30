5ef1abe1696f90a8409d6b670f9cefec00adc275
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-04-30 18:03:54 +0200

    feat: implement todo app functionality with Angular
    - Add todo interface and component logic
    - Create todo list UI with add/toggle/delete functionality
    - Add initial todos data in JSON file
    - Configure assets in angular.json for todos.json

--- frontends/angular-todos/src/app/app.component.ts
+++ frontends/angular-todos/src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-todos';
+export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+  newTodoTitle = '';
+
+  ngOnInit() {
+    // Load todos from JSON file
+    fetch('app/todos.json')
+      .then((response) => response.json())
+      .then((data) => (this.todos = data.todos));
+  }
+
+  addTodo() {
+    if (this.newTodoTitle.trim()) {
+      const newTodo: Todo = {
+        id: this.todos.length + 1,
+        title: this.newTodoTitle.trim(),
+        completed: false,
+      };
+      this.todos.push(newTodo);
+      this.newTodoTitle = '';
+    }
+  }
+
+  toggleTodo(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 }
