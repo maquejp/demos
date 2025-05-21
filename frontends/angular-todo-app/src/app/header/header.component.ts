29619cf1c151945f3dd3b52ffceab350d2b2ba2c
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-05-21 16:24:55 +0200

    feat(header): add header component with HTML and CSS

---
+++ frontends/angular-todo-app/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
