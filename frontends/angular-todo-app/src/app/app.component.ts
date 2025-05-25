24fd07fad30765c5cc1214db3ac0974b6ccb362a
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-05-25 10:58:28 +0200

    feat(footer): add footer component to enhance app layout

--- frontends/angular-todo-app/src/app/app.component.ts
+++ frontends/angular-todo-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TaskListCardComponent } from './task-list-card/task-list-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TaskListCardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, TaskListCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
