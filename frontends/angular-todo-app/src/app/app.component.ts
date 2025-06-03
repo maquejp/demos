80e7597d86e365a7ae152d0091df5bd1ba708a6a
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-06-03 14:33:05 +0200

    refactor(app): simplify app structure by removing unused components and updating styles

--- frontends/angular-todo-app/src/app/app.component.ts
+++ frontends/angular-todo-app/src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { TaskListCardComponent } from './components/task-list-card/task-list-card.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent,
-    TaskListCardComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-todo-app';
