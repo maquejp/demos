f238410e7245bfa22429aa344b69a36898098cbc
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-06-03 14:58:55 +0200

    feat(task-form): create task form component with HTML and CSS files

---
+++ frontends/angular-todo-app/src/app/components/tasks/form/form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class TaskFormComponent {}
