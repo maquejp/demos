3a70e9810448b4ec4c7db9f8c781f8699a0c230f
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-06-12 13:22:48 +0200

    feat(dependencies): add FontAwesome icons for enhanced UI in header component

--- frontends/angular-todo-app/src/app/components/header/header.component.ts
+++ frontends/angular-todo-app/src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  faUser = faUser;
 }
