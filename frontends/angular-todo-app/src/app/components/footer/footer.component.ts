ffb22d0aebf96dd6c1f1803df0fa6308a9e53db0
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-06-12 15:17:01 +0200

    feat(footer): enhance footer component with company info, quick links, and social media icons

--- frontends/angular-todo-app/src/app/components/footer/footer.component.ts
+++ frontends/angular-todo-app/src/app/components/footer/footer.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faLinkedin,
+  faXTwitter,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  faLinkedin = faLinkedin;
+  faXTwitter = faXTwitter;
+  faGithub = faGithub;
 }
