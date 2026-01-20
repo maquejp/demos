3f9659e5cf47247512f86326c2faae199dabc8ef
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-01-20 10:27:13 +0100

    feat(angular-resume-footer): bind personalInfo and enhance footer display

--- angular-resume/src/app/footer/footer.ts
+++ angular-resume/src/app/footer/footer.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PersonalInfo } from '../types';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
+  @Input() personalInfo!: PersonalInfo;
 
+  get name(): string {
+    return this.personalInfo.name;
+  }
+
+  get nameSplitInitials(): string {
+    return this.personalInfo.name
+      .split(' ')
+      .map((n) => n[0])
+      .join('');
+  }
+
+  get title(): string | undefined {
+    return this.personalInfo.title;
+  }
+
+  get location(): string | undefined {
+    return this.personalInfo.location;
+  }
+
+  get email(): string | undefined {
+    return this.personalInfo.email;
+  }
+
+  get phone(): string | undefined {
+    return this.personalInfo.phone;
+  }
+
+  get linkedin(): string | undefined {
+    return this.personalInfo.linkedin;
+  }
 }
