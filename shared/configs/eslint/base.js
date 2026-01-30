ace06388ed268cf970a8032c5fbf192c652860e6
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-01-30 11:41:36 +0100

    feat(simple-web-react-resume): :lipstick: Add eslint and tailwincss

---
+++ shared/configs/eslint/base.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export const baseConfig = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+];
